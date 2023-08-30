80da75913b9cd5af718142c295732ac79e480005
green-Desktop <appieosugood@gmail.com>
2023-08-31 02:02:42 +0800

    vector2 Rotation by angle work

--- LinearAlgebra/Vector2Function.cs
+++ LinearAlgebra/Vector2Function.cs
@@ -1,20 +1,16 @@
 ﻿using UnityEngine;
+using UnityTools.Vector2.Metrix;
 
 namespace UnityTools.Vector2
 {
     public class Vector2Function
     {
         public static UnityEngine.Vector2 RotateVector(UnityEngine.Vector2 v, float theta)
-    {
-        float[,] rotationMatrix = {
-            { Mathf.Cos(theta), -Mathf.Sin(theta) },
-            { Mathf.Sin(theta), Mathf.Cos(theta) }
-        };
-
-        float newX = v.x * rotationMatrix[0, 0] + v.y * rotationMatrix[0, 1];
-        float newY = v.x * rotationMatrix[1, 0] + v.y * rotationMatrix[1, 1];
-
-        return new UnityEngine.Vector2(newX, newY);
-    }
+        {
+            var rotationMatrix2x2 = new Metrix2x2(
+                new UnityEngine.Vector2(Mathf.Cos(theta / 180 * Mathf.PI), Mathf.Sin(theta / 180 * Mathf.PI)),
+                new UnityEngine.Vector2(-Mathf.Sin(theta / 180 * Mathf.PI), Mathf.Cos(theta / 180 * Mathf.PI)));
+            return v * rotationMatrix2x2;
+        }
     }
 }
