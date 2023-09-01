58f9fda7701369fb184c17bc8b4accd76a164d3d
green-Desktop <appieosugood@gmail.com>
2023-09-01 18:16:31 +0800

    rename vector function, add vectorWiseMul function

--- LinearAlgebra/VectorFunction.cs
+++ LinearAlgebra/VectorFunction.cs
@@ -3,7 +3,7 @@ using UnityTools.Vector2.Metrix;
 
 namespace UnityTools.Vector2
 {
-    public class Vector2Function
+    public class VectorFunction
     {
         public static UnityEngine.Vector2 RotateVector(UnityEngine.Vector2 v, float theta)
         {
@@ -11,6 +11,15 @@ namespace UnityTools.Vector2
                 new UnityEngine.Vector2(Mathf.Cos(theta / 180 * Mathf.PI), Mathf.Sin(theta / 180 * Mathf.PI)),
                 new UnityEngine.Vector2(-Mathf.Sin(theta / 180 * Mathf.PI), Mathf.Cos(theta / 180 * Mathf.PI)));
             return v * rotationMatrix2x2;
+        }
+
+        public static Vector3 ElementWiseMultiply(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(
+                v1.x * v2.x,
+                v1.y * v2.y,
+                v1.z * v2.z
+            );
         }
     }
 }
